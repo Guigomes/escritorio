42bfd72ef8d27cc2c654fd8d0e877007b48a2000
Guigomes <guigomes.ti@gmail.com>
2018-09-07 19:12:06 -0300

    deletando arquivos desnecessários

--- src/app/examples/examples.module.js
+++ src/app/examples/examples.module.js
@@ -1,21 +1,9 @@
 (function() {
-    'use strict';
+  "use strict";
 
-    angular
-        .module('app.examples', [
-            'app.examples.authentication',
-            'app.examples.calendar',
-            'app.examples.charts',
-            'app.examples.dashboards',
-            'app.examples.elements',
-            'app.examples.email',
-            'app.examples.extras',
-            'app.examples.forms',
-            'app.examples.github',
-            'app.examples.layouts',
-            'app.examples.maps',
-            'app.examples.menu',
-            'app.examples.todo',
-            'app.examples.ui'
-        ]);
+  angular.module("app.examples", [
+    "app.examples.dashboards",
+
+    "app.examples.ui"
+  ]);
 })();
